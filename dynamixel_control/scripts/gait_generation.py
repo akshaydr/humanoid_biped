d2080516bf795d9da6aea8e167b8ca5abe866e54
akshaydr <akshay.dr.11@gmail.com>
2018-07-15 12:27:26 +0530

    Added all 12 motors intit program and updated parameters. Generated basice motion.

--- dynamixel_control/scripts/gait_generation.py
+++ dynamixel_control/scripts/gait_generation.py
@@ -14,26 +14,61 @@ if __name__ == '__main__':
     flexion_left_pub = rospy.Publisher('flexion_left_value', Float64, queue_size=10)
     knee_left_pub = rospy.Publisher('knee_left_value', Float64, queue_size=10)
 
+    abduction_right_pub = rospy.Publisher('abduction_right_value', Float64, queue_size=10)
+    rotation_right_pub = rospy.Publisher('rotation_right_value', Float64, queue_size=10)
+    flexion_right_pub = rospy.Publisher('flexion_right_value', Float64, queue_size=10)
+    knee_right_pub = rospy.Publisher('knee_right_value', Float64, queue_size=10)
+
+    mid_section_pub = rospy.Publisher('mid_section_value', Float64, queue_size=10)
+    uppper_body_pub = rospy.Publisher('uppper_body_value', Float64, queue_size=10)
+
     rate = rospy.Rate(10) # 10hz
 
     while not rospy.is_shutdown():
         type = raw_input("Enter the motion gait type:")
         if (type == "init"):
-            abduction_left_pub.publish(2545.0)
+            abduction_left_pub.publish(2642.0)
             rotation_left_pub.publish(3062.0)
             flexion_left_pub.publish(1536.0)
             knee_left_pub.publish(2050.0)
+
+            abduction_right_pub.publish(2621.0)
+            rotation_right_pub.publish(2040.0)
+            flexion_right_pub.publish(3054.0)
+            knee_right_pub.publish(2031.0)
+
+            mid_section_pub.publish(2012.0)
+            uppper_body_pub.publish(2015.0)
 
         if (type == "move"):
-            sleep(1)
+            sleep(0.5)
             abduction_left_pub.publish(2545.0)
             rotation_left_pub.publish(3062.0)
-            flexion_left_pub.publish(2129.0)
-            knee_left_pub.publish(1428.0)
+            flexion_left_pub.publish(2442.0)
+            knee_left_pub.publish(1164.0)
+
+            sleep(0.5)
 
             abduction_left_pub.publish(2545.0)
             rotation_left_pub.publish(3062.0)
             flexion_left_pub.publish(1536.0)
             knee_left_pub.publish(2050.0)
+
+            sleep(2)
+
+            abduction_right_pub.publish(2621.0)
+            rotation_right_pub.publish(2040.0)
+            flexion_right_pub.publish(2081.0)
+            knee_right_pub.publish(2929.0)
+
+            sleep(0.5)
+
+            abduction_right_pub.publish(2621.0)
+            rotation_right_pub.publish(2040.0)
+            flexion_right_pub.publish(3054.0)
+            knee_right_pub.publish(2031.0)
+
+
+
 
         rate.sleep()
--- dynamixel_control/scripts/motor_drive.py
+++ dynamixel_control/scripts/motor_drive.py
@@ -21,27 +21,84 @@ knee_left_angle = JointCommand()
 knee_left_angle.unit = "raw"
 knee_left_angle.id = 9
 
-def abduction_callback(msg):
+abduction_right_angle = JointCommand()
+abduction_right_angle.unit = "raw"
+abduction_right_angle.id = 5
+
+rotation_right_angle = JointCommand()
+rotation_right_angle.unit = "raw"
+rotation_right_angle.id = 4
+
+flextion_right_angle = JointCommand()
+flextion_right_angle.unit = "raw"
+flextion_right_angle.id = 3
+
+knee_right_angle = JointCommand()
+knee_right_angle.unit = "raw"
+knee_right_angle.id = 2
+
+mid_section_angle = JointCommand()
+mid_section_angle.unit = "raw"
+mid_section_angle.id = 11
+
+uppper_body_angle = JointCommand()
+uppper_body_angle.unit = "raw"
+uppper_body_angle.id = 12
+
+def abduction_left_callback(msg):
     angle_service = rospy.ServiceProxy('joint_command', JointCommand)
     angle_service(abduction_left_angle.unit, abduction_left_angle.id, constrain(msg.data, "abduction_left"))
 
-def rotation_callback(msg):
+def rotation_left_callback(msg):
     angle_service = rospy.ServiceProxy('joint_command', JointCommand)
     angle_service(rotation_left_angle.unit, rotation_left_angle.id, constrain(msg.data, "rotation_left"))
 
-def flexion_callback(msg):
+def flexion_left_callback(msg):
     angle_service = rospy.ServiceProxy('joint_command', JointCommand)
     angle_service(flextion_left_angle.unit, flextion_left_angle.id, constrain(msg.data, "flexion_left"))
 
-def knee_callback(msg):
+def knee_left_callback(msg):
     angle_service = rospy.ServiceProxy('joint_command', JointCommand)
     angle_service(knee_left_angle.unit, knee_left_angle.id, constrain(msg.data, "flexion_left"))
 
+def abduction_right_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(abduction_right_angle.unit, abduction_right_angle.id, constrain(msg.data, "abduction_right"))
+
+def rotation_right_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(rotation_right_angle.unit, rotation_right_angle.id, constrain(msg.data, "rotation_right"))
+
+def flexion_right_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(flextion_right_angle.unit, flextion_right_angle.id, constrain(msg.data, "flexion_right"))
+
+def knee_right_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(knee_right_angle.unit, knee_right_angle.id, constrain(msg.data, "flexion_right"))
+
+def mid_section_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(mid_section_angle.unit, mid_section_angle.id, constrain(msg.data, "mid_section"))
+
+def uppper_body_callback(msg):
+    angle_service = rospy.ServiceProxy('joint_command', JointCommand)
+    angle_service(uppper_body_angle.unit, uppper_body_angle.id, constrain(msg.data, "uppper_body"))
+
 if __name__ == '__main__':
     rospy.init_node('motor_drive', anonymous=True)
-    rospy.Subscriber("flexion_left_value", Float64, flexion_callback)
-    rospy.Subscriber("abduction_left_value", Float64, abduction_callback)
-    rospy.Subscriber("knee_left_value", Float64, knee_callback)
-    rospy.Subscriber("rotation_left_value", Float64, rotation_callback)
+
+    rospy.Subscriber("flexion_left_value", Float64, flexion_left_callback)
+    rospy.Subscriber("abduction_left_value", Float64, abduction_left_callback)
+    rospy.Subscriber("knee_left_value", Float64, knee_left_callback)
+    rospy.Subscriber("rotation_left_value", Float64, rotation_left_callback)
+
+    rospy.Subscriber("flexion_right_value", Float64, flexion_right_callback)
+    rospy.Subscriber("abduction_right_value", Float64, abduction_right_callback)
+    rospy.Subscriber("knee_right_value", Float64, knee_right_callback)
+    rospy.Subscriber("rotation_right_value", Float64, rotation_right_callback)
+
+    rospy.Subscriber("mid_section_value", Float64, mid_section_callback)
+    rospy.Subscriber("uppper_body_value", Float64, uppper_body_callback)
 
     rospy.spin()
