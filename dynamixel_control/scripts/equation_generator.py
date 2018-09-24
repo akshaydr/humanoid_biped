0758aad227596b7d04cbe611d5f71c90868edd3e
akshaydr <akshay.dr.11@gmail.com>
2018-09-24 22:18:30 +0530

    Tested two  dynamixel motors. Woking fine

--- dynamixel_control/scripts/equation_generator.py
+++ dynamixel_control/scripts/equation_generator.py
@@ -6,9 +6,9 @@ from time import sleep
 from std_msgs.msg import Float64
 
 pres_time = 0.0
-total_time = 4.0
+total_time = 1.5
 initial_angle = 180.4
-peak_angle = 2000.6
+peak_angle = 1000.6
 
 def co_efficients(total_time, initial_angle, peak_angle):
     lhs = np.array([[0, 0, 1], [(total_time/2) ** 2, total_time/2, 1], [total_time ** 2, total_time, 1]])
@@ -24,12 +24,14 @@ co_efficient_values = co_efficients(total_time, initial_angle, peak_angle)
 
 if __name__ == '__main__':
     rospy.init_node('gait_generation', anonymous=True)
-    pub = rospy.Publisher('angle', Float64, queue_size=10)
+    pub1 = rospy.Publisher('knee_angle', Float64, queue_size=10)
+    pub2 = rospy.Publisher('hip_angle', Float64, queue_size=10)
     rate = rospy.Rate(10) # 10hz
 
 while (pres_time < total_time):
     angle_val = angle(co_efficient_values, pres_time)
-    pres_time += 0.01
-    pub.publish(angle_val)
+    pres_time += 0.02
+    pub1.publish(angle_val)
+    pub2.publish(angle_val)
     print (angle_val)
-    sleep(0.01)
+    sleep(0.02)
--- dynamixel_control/scripts/single_motor.py
+++ dynamixel_control/scripts/single_motor.py
@@ -35,14 +35,17 @@ LEN_PRO_PRESENT_POSITION    = 4
 PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-DXL1_ID                     = 1                 # Dynamixel#1 ID : 1
+DXL1_ID                     = 7                # Dynamixel#1 ID : 1
+DXL2_ID                     = 8                # Dynamixel#1 ID : 1
+DXL3_ID                     = 9                 # Dynamixel#1 ID : 1
+DXL4_ID                     = 10                 # Dynamixel#1 ID : 1
 BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
 TORQUE_ENABLE               = 1                 # Value for enabling the torque
 TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 100           # Dynamixel will rotate between this value
+DXL_MINIMUM_POSITION_VALUE  = 100          # Dynamixel will rotate between this value
 DXL_MAXIMUM_POSITION_VALUE  = 4000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 
@@ -50,21 +53,13 @@ index = 0
 dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
 
 
-# Initialize PortHandler instance
-# Set the port path
-# Get methods and members of PortHandlerLinux or PortHandlerWindows
 portHandler = PortHandler(DEVICENAME)
-
-# Initialize PacketHandler instance
-# Set the protocol version
-# Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
 packetHandler = PacketHandler(PROTOCOL_VERSION)
-
-# Initialize GroupSyncWrite instance
 groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_PRO_GOAL_POSITION, LEN_PRO_GOAL_POSITION)
-
-# Initialize GroupSyncRead instace for Present Position
 groupSyncRead = GroupSyncRead(portHandler, packetHandler, ADDR_PRO_PRESENT_POSITION, LEN_PRO_PRESENT_POSITION)
+
+hip_angle_val = 0.0
+knee_angle_val = 0.0
 
 # Open port
 if portHandler.openPort():
@@ -74,7 +69,6 @@ else:
     print("Press any key to terminate...")
     getch()
     quit()
-
 
 # Set port baudrate
 if portHandler.setBaudRate(BAUDRATE):
@@ -86,31 +80,30 @@ else:
     quit()
 
 
-def initialize_motor(DXL1_ID):
+def initialize_motor(ID):
     # Enable Dynamixel#1 Torque
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
     else:
-        print("Dynamixel#%d has been successfully connected" % DXL1_ID)
+        print("Dynamixel#%d has been successfully connected" % ID)
 
     # Add parameter storage for Dynamixel#1 present position value
-    dxl_addparam_result = groupSyncRead.addParam(DXL1_ID)
+    dxl_addparam_result = groupSyncRead.addParam(ID)
     if dxl_addparam_result != True:
-        print("[ID:%03d] groupSyncRead addparam failed" % DXL1_ID)
+        print("[ID:%03d] groupSyncRead addparam failed" % ID)
         quit()
 
-
-def angle_callback(msg):
+def run_motor(ID, data):
     # Allocate goal position value into byte array
-    param_goal_position = [DXL_LOBYTE(DXL_LOWORD(int(msg.data))), DXL_HIBYTE(DXL_LOWORD(int(msg.data))), DXL_LOBYTE(DXL_HIWORD(int(msg.data))), DXL_HIBYTE(DXL_HIWORD(int(msg.data)))]
+    param_goal_position = [DXL_LOBYTE(DXL_LOWORD(int(data))), DXL_HIBYTE(DXL_LOWORD(int(data))), DXL_LOBYTE(DXL_HIWORD(int(data))), DXL_HIBYTE(DXL_HIWORD(int(data)))]
 
     # Add Dynamixel#1 goal position value to the Syncwrite parameter storage
-    dxl_addparam_result = groupSyncWrite.addParam(DXL1_ID, param_goal_position)
+    dxl_addparam_result = groupSyncWrite.addParam(ID, param_goal_position)
     if dxl_addparam_result != True:
-        print("[ID:%03d] groupSyncWrite addparam failed" % DXL1_ID)
+        print("[ID:%03d] groupSyncWrite addparam failed" % ID)
         quit()
 
     # Syncwrite goal position
@@ -118,17 +111,43 @@ def angle_callback(msg):
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 
-    # Clear syncwrite parameter storage
     groupSyncWrite.clearParam()
+    # Clear syncwrite parameter storage
 
-    print msg.data
+def knee_angle_callback(msg):
+    global knee_angle_val
+    knee_angle_val = msg.data
+
+def hip_angle_callback(msg):
+    global hip_angle_val
+    hip_angle_val = msg.data
 
 if __name__ == '__main__':
-    initialize_motor(1)
+    initialize_motor(DXL1_ID) #Enter Motor ID to enable torque and add parameter storage
+    initialize_motor(DXL2_ID) #Enter Motor ID to enable torque and add parameter storage
+    initialize_motor(DXL3_ID) #Enter Motor ID to enable torque and add parameter storage
+    initialize_motor(DXL4_ID) #Enter Motor ID to enable torque and add parameter storage
+
     rospy.init_node('motor_drive', anonymous=True)
-    rospy.Subscriber("angle", Float64, angle_callback)
-    rospy.spin()
- 
+    rospy.Subscriber("knee_angle", Float64, knee_angle_callback)
+    rospy.Subscriber("hip_angle", Float64, hip_angle_callback)
+
+    rate = rospy.Rate(60)
+
+while not rospy.is_shutdown():
+    print ("/n Knee_angle =")
+    print(knee_angle_val)
+    print ("Hip_angle =")
+    print(hip_angle_val)
+
+    run_motor(DXL1_ID, knee_angle_val)
+    run_motor(DXL2_ID, hip_angle_val)
+    # run_motor(DXL3_ID, msg.data)
+    # run_motor(DXL4_ID, msg.data)
+
+    rate.sleep()
+
+
 # # Clear syncread parameter storage
 # groupSyncRead.clearParam()
 #
