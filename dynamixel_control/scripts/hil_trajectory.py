5822f325d166bbd634b94f56f840a2dabc92f509
akshaydr <akshay.dr.11@gmail.com>
2019-05-03 14:01:32 +0530

    Added walking gait bag file

--- dynamixel_control/scripts/hil_trajectory.py
+++ dynamixel_control/scripts/hil_trajectory.py
@@ -5,45 +5,14 @@ import rospy
 import numpy as np
 from time import sleep
 from std_msgs.msg import Float64
+from std_msgs.msg import String
 
-UDP_IP_ADDRESS = "192.168.2.101"
-UDP_PORT_NO = 8001
-
-UDP_IP = "192.168.7.3"
-UDP_PORT = 1234
-
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
-
-
-if __name__ == '__main__':
-    rospy.init_node('gait_generation', anonymous=True)
-    pub1 = rospy.Publisher('rotation_angle_r', Float64, queue_size=10)
-    pub2 = rospy.Publisher('abduction_angle_r', Float64, queue_size=10)
-    pub3 = rospy.Publisher('hip_angle_r', Float64, queue_size=10)
-    pub4 = rospy.Publisher('knee_angle_r', Float64, queue_size=10)
-    pub5 = rospy.Publisher('ankle_angle_r', Float64, queue_size=10)
-    pub6 = rospy.Publisher('ankle_twist_angle_r', Float64, queue_size=10)
-
-    pub7 = rospy.Publisher('rotation_angle_l', Float64, queue_size=10)
-    pub8 = rospy.Publisher('abduction_angle_l', Float64, queue_size=10)
-    pub9 = rospy.Publisher('hip_angle_l', Float64, queue_size=10)
-    pub10 = rospy.Publisher('knee_angle_l', Float64, queue_size=10)
-    pub11 = rospy.Publisher('ankle_angle_l', Float64, queue_size=10)
-    pub12 = rospy.Publisher('ankle_twist_angle_l', Float64, queue_size=10)
-
-    rate = rospy.Rate(1000)
-
-while not rospy.is_shutdown():
-    data, addr = serverSock.recvfrom(1024)
-    data = data.translate(None, '\x00')
+def data_callback(msg):
+    data = msg.data
     print data
     val = data.split(",")
     rotation_right, abduc_val_right, hip_val_right, knee_val_right, ankle_val_right, ankle_twist_right, rotation_left, abduc_val_left, hip_val_left,  knee_val_left, ankle_val_left, ankle_twist_left = data.split(",")
     print rotation_right, abduc_val_right, hip_val_right, knee_val_right, ankle_val_right, ankle_twist_right, rotation_left, abduc_val_left, hip_val_left,  knee_val_left, ankle_val_left, ankle_twist_left
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(data, (UDP_IP, UDP_PORT))
 
     rotation_right = float(rotation_right)
     abduc_val_right = float(abduc_val_right)
@@ -74,3 +43,27 @@ while not rospy.is_shutdown():
     pub10.publish(knee_val_left)
     pub11.publish(ankle_val_left)
     pub12.publish(ankle_twist_left)
+
+
+if __name__ == '__main__':
+    rospy.init_node('gait_generation', anonymous=True)
+    pub1 = rospy.Publisher('rotation_angle_r', Float64, queue_size=10)
+    pub2 = rospy.Publisher('abduction_angle_r', Float64, queue_size=10)
+    pub3 = rospy.Publisher('hip_angle_r', Float64, queue_size=10)
+    pub4 = rospy.Publisher('knee_angle_r', Float64, queue_size=10)
+    pub5 = rospy.Publisher('ankle_angle_r', Float64, queue_size=10)
+    pub6 = rospy.Publisher('ankle_twist_angle_r', Float64, queue_size=10)
+
+    pub7 = rospy.Publisher('rotation_angle_l', Float64, queue_size=10)
+    pub8 = rospy.Publisher('abduction_angle_l', Float64, queue_size=10)
+    pub9 = rospy.Publisher('hip_angle_l', Float64, queue_size=10)
+    pub10 = rospy.Publisher('knee_angle_l', Float64, queue_size=10)
+    pub11 = rospy.Publisher('ankle_angle_l', Float64, queue_size=10)
+    pub12 = rospy.Publisher('ankle_twist_angle_l', Float64, queue_size=10)
+
+    rospy.Subscriber("speedgoat_data", String, data_callback)
+
+    rate = rospy.Rate(1000)
+
+while not rospy.is_shutdown():
+    rate.sleep()
